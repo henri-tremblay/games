47cdcb8b320dd72a6c71c56c5f982aaca4403fe5
Henri Tremblay <henri.tremblay@gmail.com>
2024-03-03 22:39:24 -0400

    Before the buffer strategy

---
+++ src/main/java/pro/tremblay/pong/App.java
@@ -0,0 +1,217 @@
+package pro.tremblay.pong;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.random.RandomGenerator;
+
+// Collision balle avec bas à corriger
+// Rebondir sur les palettes
+// Augmenter la balle de vitesse à chaque 10 secondes xxxx
+// Écrire le pointage
+// Utiliser un tampon d'affichage
+
+abstract class Sprite {
+    protected double x, y;
+    protected double vx, vy;
+
+    public void position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void speed(double vx, double vy) {
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    public void move() {
+        x += vx;
+        y += vy;
+    }
+
+    public abstract void draw(Graphics g);
+}
+
+class Pad extends Sprite {
+    static final int WIDTH = 30;
+    static final int HEIGHT = 150;
+
+    @Override
+    public void move() {
+        super.move();
+        if (y < 0) {
+            y  = 0;
+        } else if (y + HEIGHT > App.SCREEN_HEIGHT) {
+            y = App.SCREEN_HEIGHT - HEIGHT;
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect((int) x, (int) y, WIDTH, HEIGHT);
+
+    }
+
+}
+
+class Ball extends Sprite {
+    static final int DIAMETER = 50;
+
+    @Override
+    public void move() {
+        super.move();
+        if (y < 0) {
+            y  = 0;
+            vy = -vy;
+        } else if (y + DIAMETER > App.SCREEN_HEIGHT) {
+            y = App.SCREEN_HEIGHT - DIAMETER;
+            vy = -vy;
+        }
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval((int) x, (int) y, DIAMETER, DIAMETER);
+    }
+}
+
+public class App {
+    static final int SCREEN_WIDTH = 1024;
+    static final int SCREEN_HEIGHT = 768;
+    static double ballSpeed = 15;
+    static final int BUFFERS = 2;
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.start();
+    }
+
+    private final Pad left = new Pad();
+    private final Pad right = new Pad();
+    private final Ball ball = new Ball();
+
+    private void start() {
+
+        JFrame frame = new JFrame("Pong Édouard");
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation((d.width- SCREEN_WIDTH) / 2, (d.height - SCREEN_HEIGHT) / 2);
+        frame.createBufferStrategy(2);
+
+        BufferStrategy bufferStrategy = frame.getBufferStrategy();
+
+        initPads();
+        initBall();
+
+
+        // Offscreen buffer
+        BufferedImage image = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+
+        JPanel panel = new JPanel() {
+
+            @Override
+            public void paint(Graphics g) {
+//                g.drawImage(image, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+            }
+
+        };
+        panel.setFocusable(true);
+        panel.addKeyListener(new KeyAdapter() {
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP -> {
+                        right.vy = -10;
+                    }
+                    case KeyEvent.VK_DOWN -> {
+                        right.vy = 10;
+                    }
+                    case KeyEvent.VK_W -> {
+                        left.vy = -10;
+                    }
+                    case KeyEvent.VK_S -> {
+                        left.vy = 10;
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP -> {
+                        right.vy = 0;
+                    }
+                    case KeyEvent.VK_DOWN -> {
+                        right.vy = 0;
+                    }
+                    case KeyEvent.VK_W -> {
+                        left.vy = 0;
+                    }
+                    case KeyEvent.VK_S -> {
+                        left.vy = 0;
+                    }
+                }
+            }
+        });
+
+//        frame.getContentPane().add(panel);
+        frame.setVisible(true);
+
+        AtomicInteger ballLevel = new AtomicInteger(0);
+
+        new Timer(100, e -> {
+            if (ballLevel.incrementAndGet() % 1000 == 0) {
+                double plusOneRatio = (ballSpeed + 1) / ballSpeed;
+                ball.speed(ball.vx * plusOneRatio, ball.vy * plusOneRatio);
+                ballSpeed++;
+            }
+            left.move();
+            right.move();
+            ball.move();
+
+            Graphics g = image.getGraphics();
+
+            fondEcran(g, Color.BLACK);
+
+            left.draw(g);
+            right.draw(g);
+            ball.draw(g);
+
+            g.dispose();
+
+            panel.repaint();
+        }).start();
+    }
+
+    private void fondEcran(Graphics g, Color color) {
+        g.setColor(color);
+        g.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    }
+
+    private void initBall() {
+        RandomGenerator rand = RandomGenerator.getDefault();
+        double angle = rand.nextDouble(2 * Math.PI);
+        ball.position((SCREEN_WIDTH - Ball.DIAMETER) / 2, (SCREEN_HEIGHT - Ball.DIAMETER) / 2);
+        double ballvx = (Math.cos(angle) * ballSpeed);
+        double ballvy = (Math.sin(angle) * ballSpeed);
+        ball.speed(ballvx, ballvy);
+    }
+
+    private void initPads() {
+        int padY = (SCREEN_HEIGHT - Pad.HEIGHT) / 2;
+        left.position(50, padY);
+        right.position(SCREEN_WIDTH - 50 - Pad.WIDTH, padY);
+        left.speed(0, 0);
+        right.speed(0, 0);
+    }
+}
---
+++ src/test/java/pro/tremblay/pong/AppTest.java
@@ -0,0 +1,13 @@
+package pro.tremblay.pong;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertThat(true).isTrue();
+    }
+}
