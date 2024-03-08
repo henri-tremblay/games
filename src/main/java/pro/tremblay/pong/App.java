23f2da692a045248de94b3dc4ba700ecdc23c675
Henri Tremblay <henri.tremblay@gmail.com>
2024-03-07 22:11:10 -0400

    Add score

--- src/main/java/pro/tremblay/pong/App.java
+++ src/main/java/pro/tremblay/pong/App.java
@@ -4,14 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferStrategy;
+import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.random.RandomGenerator;
 
 // Collision balle avec bas à corriger
 // Rebondir sur les palettes
 // Augmenter la balle de vitesse à chaque 10 secondes xxxx
-// Écrire le pointage
+// Écrire le pointage xxx
 // Utiliser un tampon d'affichage xxx
 
 abstract class Sprite {
@@ -33,12 +37,23 @@ abstract class Sprite {
         y += vy;
     }
 
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Sprite.class.getSimpleName() + "[", "]")
+                .add("x=" + x)
+                .add("y=" + y)
+                .add("vx=" + vx)
+                .add("vy=" + vy)
+                .toString();
+    }
+
     public abstract void draw(Graphics g);
 }
 
 class Pad extends Sprite {
     static final int WIDTH = 30;
     static final int HEIGHT = 150;
+    static final int INITIAL_SPEED = 15;
 
     @Override
     public void move() {
@@ -81,11 +96,26 @@ class Ball extends Sprite {
     }
 }
 
+class Score extends Sprite {
+    int left, right;
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        Font currentFont = g.getFont();
+        Font newFont = currentFont.deriveFont(currentFont.getSize() * 1.4F);
+        g.setFont(newFont);
+
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.drawString(left + " " + right, (int) x, (int) y);
+    }
+}
+
 public class App {
     static final int SCREEN_WIDTH = 1024;
     static final int SCREEN_HEIGHT = 768;
     static double ballSpeed = 15;
-    static final int BUFFERS = 2;
 
     public static void main(String[] args) {
         App app = new App();
@@ -95,27 +125,37 @@ public class App {
     private final Pad left = new Pad();
     private final Pad right = new Pad();
     private final Ball ball = new Ball();
+    private final Score score = new Score();
 
     private void start() {
 
         JFrame frame = new JFrame("Pong Édouard");
-        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+        frame.setSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        frame.setLocation((d.width- SCREEN_WIDTH) / 2, (d.height - SCREEN_HEIGHT) / 2);
+        frame.setUndecorated(true);
 
         initPads();
         initBall();
+        initScore();
+
+        frame.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                frame.setTitle("Pong Édouard (" + e.getX() + "," + e.getY());
+            }
+        });
 
         frame.addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyPressed(KeyEvent e) {
                 switch (e.getKeyCode()) {
-                    case KeyEvent.VK_UP -> right.vy = -10;
-                    case KeyEvent.VK_DOWN -> right.vy = 10;
-                    case KeyEvent.VK_W -> left.vy = -10;
-                    case KeyEvent.VK_S -> left.vy = 10;
+                    case KeyEvent.VK_UP -> right.vy = -Pad.INITIAL_SPEED;
+                    case KeyEvent.VK_DOWN -> right.vy = Pad.INITIAL_SPEED;
+                    case KeyEvent.VK_W -> left.vy = -Pad.INITIAL_SPEED;
+                    case KeyEvent.VK_S -> left.vy = Pad.INITIAL_SPEED;
                 }
             }
 
@@ -125,6 +165,7 @@ public class App {
                     case KeyEvent.VK_UP, KeyEvent.VK_DOWN -> right.vy = 0;
                     case KeyEvent.VK_W, KeyEvent.VK_S -> left.vy = 0;
                     case KeyEvent.VK_SPACE -> initBall();
+                    case KeyEvent.VK_ESCAPE -> System.exit(0);
                 }
             }
         });
@@ -137,28 +178,63 @@ public class App {
         AtomicInteger ballLevel = new AtomicInteger(0);
 
         new Timer(100, e -> {
-            if (ballLevel.incrementAndGet() % 1000 == 0) {
-                double plusOneRatio = (ballSpeed + 1) / ballSpeed;
+            if (ballLevel.incrementAndGet() % 50 == 0) {
+                double plusOneRatio = (ballSpeed + 2) / ballSpeed;
                 ball.speed(ball.vx * plusOneRatio, ball.vy * plusOneRatio);
                 ballSpeed++;
             }
             left.move();
             right.move();
+
             ball.move();
+
+            collisionLeft();
+            collisionRight();
+
+            if (ball.x <= 50) {
+                score.right++;
+                initBall();
+            } else if (ball.x > SCREEN_WIDTH - 50) {
+                score.left++;
+                initBall();
+            }
 
             Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
 
             try {
                 fondEcran(g, Color.BLACK);
+                ligneDuCentre(g);
 
                 left.draw(g);
                 right.draw(g);
                 ball.draw(g);
+                score.draw(g);
             } finally {
                 g.dispose();
             }
             bufferStrategy.show();
         }).start();
+    }
+
+    private void collisionRight() {
+        collision(right);
+    }
+
+    private void collisionLeft() {
+        collision(left);
+    }
+
+    private void collision(Pad pad) {
+        Rectangle2D.Double padRect = new Rectangle2D.Double(pad.x, pad.y, Pad.WIDTH, Pad.HEIGHT);
+        Rectangle2D.Double ballRect = new Rectangle2D.Double(ball.x, ball.y, Ball.DIAMETER, Ball.DIAMETER);
+        if (padRect.intersects(ballRect)) {
+            ball.speed(-ball.vx, ball.vy);
+        }
+    }
+
+    private void ligneDuCentre(Graphics2D g) {
+        g.setColor(Color.YELLOW);
+        g.drawLine(SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, SCREEN_HEIGHT);
     }
 
     private void fondEcran(Graphics g, Color color) {
@@ -167,12 +243,26 @@ public class App {
     }
 
     private void initBall() {
-        RandomGenerator rand = RandomGenerator.getDefault();
-        double angle = rand.nextDouble(2 * Math.PI);
+        double angle = findGoodAngle();
         ball.position((SCREEN_WIDTH - Ball.DIAMETER) / 2, (SCREEN_HEIGHT - Ball.DIAMETER) / 2);
         double ballvx = (Math.cos(angle) * ballSpeed);
         double ballvy = (Math.sin(angle) * ballSpeed);
         ball.speed(ballvx, ballvy);
+    }
+
+    private double findGoodAngle() {
+        RandomGenerator rand = RandomGenerator.getDefault();
+        double angle = rand.nextDouble(2 * Math.PI);
+        while(!goodAngle(angle)) {
+            angle = rand.nextDouble(2 * Math.PI);
+        }
+        return angle;
+    }
+
+    private boolean goodAngle(double angle) {
+        return angle < Math.PI / 4 ||
+                (angle < 5 * Math.PI / 4 && angle > 3 * Math.PI / 4) ||
+                angle >  7 * Math.PI / 4;
     }
 
     private void initPads() {
@@ -181,5 +271,9 @@ public class App {
         right.position(SCREEN_WIDTH - 50 - Pad.WIDTH, padY);
         left.speed(0, 0);
         right.speed(0, 0);
+    }
+
+    private void initScore() {
+        score.position(SCREEN_WIDTH / 2 - 15, 40);
     }
 }
